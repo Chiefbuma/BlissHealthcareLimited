ad8bd68f9ea8d8405575675d0e3d3f678c6180c0
Chiefbuma <jorgebuma.jb@gmail.com>
2024-03-12 17:17:18 +0300

    new

---
+++ App/conect.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import mysql.connector
+import sqlalchemy
+
+# Streamlit app starts here
+st.title("MySQL Database Connection Example")
+
+conn= st.connection('mysql',type='sql')
+
+
+# Example: Query to select all columns from the facilities table
+location_df = conn.query("SELECT * FROM facilities")
+
+# Display the result in Streamlit
+st.write("Facilities Table:")
+st.write(location_df)
+
+# ... rest of your code ...
+
+
