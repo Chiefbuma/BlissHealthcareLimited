a7a7fcf14bde30d677ba27ce5e07f0f01b17694e
Chiefbuma <122814097+Chiefbuma@users.noreply.github.com>
2024-03-13 14:04:32 +0300

    Update Trail.py

--- Trail.py
+++ Trail.py
@@ -8,8 +8,6 @@ query = 'SELECT * FROM facilities;'
 df = conn.query(query, ttl=600)
 
 # Display the DataFrame.
-<<<<<<< HEAD
 st.write(df)
-=======
-st.write(df)
->>>>>>> 2d397cc3f6dd221c854968d35475bade2865f4a6
+
+
