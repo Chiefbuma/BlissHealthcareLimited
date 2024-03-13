e140f9fc02949ab4b840b2d1c457da236bc44537
Chiefbuma <jorgebuma.jb@gmail.com>
2024-03-13 09:41:00 +0300

    new

--- Trail.py
+++ Trail.py
@@ -2,12 +2,12 @@ import streamlit as st
 import os
 
 # Everything is accessible via the st.secrets dict:
-st.write("DB username:", st.secrets["DB_USERNAME"])
-st.write("DB password:", st.secrets["DB_TOKEN"])
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
 
 
 # And the root-level secrets are also accessible as environment variables:
 st.write(
     "Has environment variables been set:",
-    os.environ["DB_USERNAME"] == st.secrets["DB_USERNAME"],
+    os.environ["db_username"] == st.secrets["db_username"],
 )
