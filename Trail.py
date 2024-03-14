c9542a64d6f8b228101854ca93290c18450564df
Chiefbuma <122814097+Chiefbuma@users.noreply.github.com>
2024-03-14 12:16:39 +0300

    Update Trail.py

--- Trail.py
+++ Trail.py
@@ -3,7 +3,7 @@ import pymssql
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-conn = pymssql.connect(server='127.0.0.1', user='sa', password='buluma', database='mydb', port=1433)
+conn = pymssql.connect(server='fe80::4425:f57:3463:4d15%10', user='sa', password='buluma', database='mydb', port=1433)
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
