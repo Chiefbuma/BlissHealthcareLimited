0ee1b7eace8eff5fb5f21209a12c2524e3385f2a
Chiefbuma <jorgebuma.jb@gmail.com>
2024-03-14 10:25:18 +0300

    mew

--- Trail.py
+++ Trail.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pyodbc
 
+
+
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-@st.cache_resource
+
 def init_connection():
     return pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
@@ -20,7 +22,7 @@ conn = init_connection()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
+
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
--- conect.py
+++ conect.py
@@ -1,20 +1,7 @@
-import streamlit as st
-import mysql.connector
-import sqlalchemy
-
-# Streamlit app starts here
-st.title("MySQL Database Connection Example")
-
-conn= st.connection('mysql',type='sql')
-
-
-# Example: Query to select all columns from the facilities table
-location_df = conn.query("SELECT * FROM facilities")
-
-# Display the result in Streamlit
-st.write("Facilities Table:")
-st.write(location_df)
-
-# ... rest of your code ...
-
-
+import pyodbc
+import sqlserverport
+servername = 'myserver'
+serverspec = '{0},{1}'.format(
+    servername,
+    sqlserverport.lookup(servername, 'SQLEXPRESS'))
+conn = pyodbc.connect('DRIVER=ODBC Driver 17 for SQL Server;SERVER={};...'.format(serverspec))
