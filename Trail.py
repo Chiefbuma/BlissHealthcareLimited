fa7f6b25ee773c51ac7c865bba585eb07e3ed9e5
Chiefbuma <jorgebuma.jb@gmail.com>
2024-03-14 12:15:18 +0300

    de

--- Trail.py
+++ Trail.py
@@ -3,7 +3,7 @@ import pymssql
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-conn = pymssql.connect(server='DESKTOP-7OABLEP\SQLEXPRESS', user='sa', password='buluma', database='mydb', port=1433)
+conn = pymssql.connect(server='127.0.0.1', user='sa', password='buluma', database='mydb', port=1433)
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
