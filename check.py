46a987376d73e270c468058447948428690e6d6b
Chiefbuma <jorgebuma.jb@gmail.com>
2024-03-20 12:52:43 +0300

    new

--- check.py
+++ check.py
@@ -1,210 +1,102 @@
-
 import streamlit as st
-from st_login_form import login_form
-from st_supabase_connection import SupabaseConnection
-from supabase import create_client, Client
+import streamlit_shadcn_ui as ui
 import pandas as pd
-from datetime import datetime, timedelta
-from IPython.display import display
-import calendar
 import numpy as np
-import plotly.express as px
-from IPython.display import HTML
-from office365.sharepoint.client_context import ClientContext
-from office365.runtime.auth.authentication_context import AuthenticationContext
-import streamlit_option_menu as option_menu
-import plotly.graph_objects as go
-# Set the page configuration
+from local_components import card_container
+from streamlit_shadcn_ui import slider, input, textarea, radio_group, switch
+
+# with open("docs/introduction.md", "r") as f:
+#     st.markdown(f.read())
+
+# ui.date_picker()
+
+st.header("Streamlit Shadcn UI")
+ui.badges(badge_list=[("shadcn", "default"), ("in", "secondary"), ("streamlit", "destructive")], class_name="flex gap-2", key="main_badges1")
+st.caption("A Streamlit component library for building beautiful apps easily. Bring the power of Shadcn UI to your Streamlit apps!")
+st.caption("Get started with pip install streamlit-shadcn-ui")
 
 
+with ui.element("div", className="flex gap-2", key="buttons_group1"):
+    ui.element("button", text="Get Started", className="btn btn-primary", key="btn1")
+    ui.element("link_button", text="Github", url="https://github.com/ObservedObserver/streamlit-shadcn-ui", variant="outline", key="btn2")
 
-st.set_page_config(page_title="My Streamlit App", layout="wide")
+st.subheader("Dashboard")
 
-def home():
-    st.session_state.is_authenticated = False 
-    
-    col1, col2 = st.columns([2,1])
-    with col1:
-        menu = ["Login", "Sign up", "Log Out"]
-        choice = st.sidebar.selectbox("", menu)
+ui.tabs(options=['Overview', 'Analytics', 'Reports', 'Notifications'], default_value='Overview', key="main_tabs")
 
-        form_container = st.empty()
-        with form_container :
-            # Initialize connection.
-            # Uses st.cache_resource to only run once.
-            @st.cache_resource
-            def init_connection():
-                url = "https://effdqrpabawzgqvugxup.supabase.co"
-                key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmZmRxcnBhYmF3emdxdnVneHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MTQ1NDYsImV4cCI6MjAyNjA5MDU0Nn0.Dkxicm9oaLR5rm-SWlvGfV5OSZxFrim6x8-QNnc2Ua8"
-                return create_client(url, key)
+ui.date_picker(key="date_picker1")
 
-            supabase = init_connection()
-            
-            response = supabase.from_('facilities').select('*').execute()
-            location_df = pd.DataFrame(response.data)
+cols = st.columns(3)
+with cols[0]:
+    # with ui.card():
+    #     ui.element()
+    ui.card(title="Total Revenue", content="$45,231.89", description="+20.1% from last month", key="card1").render()
+with cols[1]:
+    ui.card(title="Subscriptions", content="+2350", description="+180.1% from last month", key="card2").render()
+with cols[2]:
+    ui.card(title="Sales", content="+12,234", description="+19% from last month", key="card3").render()
 
-            # Perform query.
-            # Uses st.cache_data to only rerun when the query changes or after 10 min.
-            @st.cache_resource(ttl=600)
-            def create_usertable():
-                try:
-                    # Assuming `supabase` is your Supabase client
-                    query = """
-                    CREATE TABLE IF NOT EXISTS users (
-                        staffnumber INTEGER NOT NULL,
-                        password TEXT NOT NULL,
-                        location TEXT NOT NULL,
-                        region TEXT NOT NULL
-                    );
-                    """
-                    supabase.query(query)
-                    return True
+def generate_sales_data():
+    np.random.seed(0)  # For reproducible results
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    sales = np.random.randint(1000, 5000, size=len(months))
+    return pd.DataFrame({'Month': months, 'Sales': sales})
 
-                except Exception as e:
-                    print(f"Error creating usertable: {e}")
-                    return False
-        
-                            
-            def add_userdata(staffnumber, password, location, region,supabase):
-                try:
-                    # Insert a new record into the 'users' table
-                    supabase.table('users').insert({
-                        'staffnumber': staffnumber,
-                        'password': password,
-                        'location': location,
-                        'region': region
-                    }).execute()
-                    return True
-                except Exception as e:
-                    print(f"Error adding userdata: {e}")
-                    return False
-            
-            def get_facilities(staffnumber):
-                # Perform a Supabase query to fetch data from the 'users' table
-                response = supabase.from_('users').select('*').eq('staffnumber', staffnumber).execute()
-                login_df = response.data
-                return login_df
-           
-            def login_user(staffnumber, password):
-                try:
-                    # Query the 'users' table using Supabase client
-                    query = f"SELECT * FROM users WHERE staffnumber = {staffnumber} AND password = '{password}'"
-                    st.write("Query:", query)  # Print the query for debugging
-                    result = supabase.query(query).execute()
-                    
+with card_container(key="chart1"):
+    st.vega_lite_chart(generate_sales_data(), {
+        'mark': {'type': 'bar', 'tooltip': True, 'fill': 'rgb(173, 250, 29)', 'cornerRadiusEnd': 4 },
+        'encoding': {
+            'x': {'field': 'Month', 'type': 'ordinal'},
+            'y': {'field': 'Sales', 'type': 'quantitative', 'axis': {'grid': False}},
+        },
+    }, use_container_width=True)
 
-                    # Fetch location and region based on staffnumber
-                    facilities_df = get_facilities(staffnumber)
-                    print("Facilities DF:", facilities_df)  # Print the facilities dataframe for debugging
+# Sample data
+data = [
+    {"invoice": "INV001", "paymentStatus": "Paid", "totalAmount": 500, "paymentMethod": "Credit Card"},
+    {"invoice": "INV002", "paymentStatus": "Unpaid", "totalAmount": 200, "paymentMethod": "Cash"},
+    {"invoice": "INV003", "paymentStatus": "Paid", "totalAmount": 150, "paymentMethod": "Debit Card"},
+    {"invoice": "INV004", "paymentStatus": "Unpaid", "totalAmount": 350, "paymentMethod": "Credit Card"},
+    {"invoice": "INV005", "paymentStatus": "Paid", "totalAmount": 400, "paymentMethod": "PayPal"},
+    # Add more records as needed
+]
 
-                    if facilities_df:
-                        location = facilities_df[0]['location']
-                        region = facilities_df[0]['region']
+# Creating a DataFrame
+invoice_df = pd.DataFrame(data)
 
-                        # Return the result, location, and region
-                        return result, location, region
-                    else:
-                        # No facilities found for the staffnumber
-                        return None, None, None
-                except Exception as e:
-                    # Log the error and return None for all values
-                    print(f"Error logging in user: {e}")
-                    return None, None, None
-                
-                
-            response = supabase.from_('facilities').select('*').execute()
-            location_df = pd.DataFrame(response.data)
-            location_names = location_df["Location"]
-            
-            
-            
-             # log in app
-             
-            if choice == "Log Out":
-                st.subheader("Log Out")
+with card_container(key="table1"):
+    ui.table(data=invoice_df, maxHeight=300)
 
-            elif choice == "Login":
-                with st.form("Login Form"):
-                    st.write("Login Form")
-                    staffnumber = st.text_input("Staffnumber")
-                    password = st.text_input("Password", type='password')
-                    load = st.form_submit_button("Login")
-                    facilities_df = get_facilities(staffnumber)
-                    
-                    location = facilities_df['location'].iloc[0]
-                    region = facilities_df['region'].iloc[0]
 
-                    if "logged_in" not in st.session_state:
-                        st.session_state.logged_in= False
+ui_result = ui.button("Button", key="btn")
+st.write("UI Button Clicked:", ui_result)
 
-                    if load or st.session_state.logged_in:
-                        st.session_state.logged_in = False
-                        result, location, region = login_user(staffnumber, password)
-                        if result:
-                            st.success("Logged In successfully")
-                            st.write(f"Location: {location}, Region: {region}")
-                            st.session_state.is_authenticated = True
-                            st.session_state["logged_in"] = True
-                            form_container.empty()
-                        else:
-                            st.warning("Invalid credentials. Please try again.")
 
-            elif choice == "Sign up":
-                with st.form("Sign-up Form"):
-                    st.write("Sign-up Form")
-                    staffnumber = st.text_input("Staffnumber")
-                    password = st.text_input("Password", type='password')
-                    location = st.selectbox("Select Location", location_names)
-                    selected_location_row = location_df[location_df['Location'] == location]
-                    # Filter location_df based on selected_location
-                    region_options = selected_location_row['Region'].tolist() if not selected_location_row.empty else []
-                    region = st.selectbox("Select Region", region_options)
-                    if st.form_submit_button("Sign up"):
-                        if add_userdata(staffnumber, password, location, region,supabase):
-                            st.success("You have created a new account")
-                            st.session_state["logged_in"] = True
-                            st.session_state.is_authenticated = True
-                            form_container.empty()
-                        else:
-                            st.warning("Failed to create a new account. Please try again.")
+# Slider Component
+slider_value = slider(default_value=[20], min_value=0, max_value=100, step=2, label="Select a Range", key="slider1")
+st.write("Slider Value:", slider_value)
 
-    if st.session_state.is_authenticated:
-            form_container.empty()
-            def fraction_of_days_in_month(given_date):
+# Input Component
+input_value = input(default_value="Hello, Streamlit!", type='text', placeholder="Enter text here", key="input1")
+st.write("Input Value:", input_value)
 
-                # Convert the input date string to a datetime object
-                given_date = pd.to_datetime(given_date, format='%Y-%m-%d')
-                
-                # Extract the month and year from the given date
-                month = given_date.month
-                year = given_date.year
-                
-                # Find the number of days in the month
-                days_in_month = calendar.monthrange(year, month)[1]
-                
-                # Calculate the fraction of days passed with two decimal places
-                fraction_passed = round(given_date.day / days_in_month, 2)
-                
-                return fraction_passed
+# Textarea Component
+textarea_value = textarea(default_value="Type your message here...", placeholder="Enter longer text", key="textarea1")
+st.write("Textarea Value:", textarea_value)
 
-with st.sidebar:
-    #st.image("Dashboard/logo.png", caption="Bliss Healthcare")
-    selected_page = option_menu.option_menu(
-        menu_title='DASHBOARDS',
-        options=['Medical centre Dashboard', 'Region Dashboard', 'Departments Dashboard', "Maintenance Dashboard", 'Summary Dashboard', 'Account'],
-        icons=['house-fill', 'receipt', 'receipt', 'receipt', 'receipt', 'person-circle'],
-        menu_icon='house-fill',
-        default_index=0,
-        styles={
-            "container": {"padding": "15", "background-color": {"grey": "black", "font-size": "10px"}},
-            "nav-link": {"color": "Blck", "font-size": "13px", "text-align": "left"},
-            "nav-link-selected": {"background-color": "Black"}
-        }
-    )
+# Radio Group Component
+radio_options = [
+    {"label": "Option A", "value": "A", "id": "r1"},
+    {"label": "Option B", "value": "B", "id": "r2"},
+    {"label": "Option C", "value": "C", "id": "r3"}
+]
+radio_value = radio_group(options=radio_options, default_value="B", key="radio1")
+st.write("Selected Radio Option:", radio_value)
 
-if st.session_state.get('selected_page'):
-    selected_page = st.session_state['selected_page']
-if selected_page == "Medical centre Dashboard":
-    home()
-else:
-    pass
+# Switch Component
+switch_value = switch(default_checked=True, label="Toggle Switch", key="switch1")
+st.write("Switch is On:", switch_value)
+
+st.subheader("Alert Dialog")
+trigger_btn = ui.button(text="Trigger Button", key="trigger_btn")
+ui.alert_dialog(show=trigger_btn, title="Alert Dialog", description="This is an alert dialog", confirm_label="OK", cancel_label="Cancel", key="alert_dialog1")
