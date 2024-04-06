ae961fd937bd63a668b09f07645bafc49c1d2ae6
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-06 13:07:44 +0300

    we

--- medical.py
+++ medical.py
@@ -372,7 +372,7 @@ def app():
                                     performance_total["Projected_Footfalls"]]
                         ,fill_color=[
                             ['rgba(0, 0, 82, 1)' if j == 0 else 'white'
-                            if i >= len(performance_total) - 6 and i != len(performance_total) - 1 
+                            if i >= len(performance_total) - 6 and j != len(performance_total) - 1 
                             else 'lightgrey' for i, j in enumerate(range(len(performance_total)))]
 
 
