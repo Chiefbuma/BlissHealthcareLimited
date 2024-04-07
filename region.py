09f6220ee6455a8f59854d1c8767c6a0f2ffcdf6
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-07 16:13:29 +0300

    new

--- region.py
+++ region.py
@@ -328,10 +328,20 @@ def app():
             # The above code is formatting the columns in a DataFrame called `performance_df`. It is
             # applying specific formatting to the numerical values in the columns to make them more
             # readable and presentable.
-            performance_df['MTD_Budget_Revenue'] = (performance_df['MTD_Budget_Revenue'] * fraction_passed).round(0)
-
-            performance_df['MTD_Budget_Footfall']=(performance_df['MTD_Budget_Footfall']*fraction_passed).round(0)
+            performance_df['MTD_Budget_Revenue'] = (performance_df['MTD_Budget_Revenue'].sum() * fraction_passed).round(0)
+            performance_df['MTD_Budget_Footfall']=(performance_df['MTD_Budget_Footfall'].sum()*fraction_passed).round(0)
             # Add a new column %Arch_FF as the percentage of MTD_Actual_Footfall to MTD_Budget_Footfall
+            performance_df['MTD_Actual_Footfall'] = (performance_df['MTD_Actual_Footfall'].sum() * fraction_passed).round(0)
+            performance_df['MTD_Actual_Revenue']=(performance_df['MTD_Actual_Revenue'].sum()*fraction_passed).round(0)
+            
+            performance_df['Projected_Revenue'] = (performance_df['MTD_Budget_Revenue'].sum() * fraction_passed).round(0)
+            performance_df['Projected_Revenue']=(performance_df['MTD_Budget_Footfall'].sum()*fraction_passed).round(0)
+
+            
+            performance_df['Total_Revenue_Budget'] = (performance_df['Total_Revenue_Budget'].sum() * fraction_passed).round(0)
+            performance_df['Total_Footfall_Budget']=(performance_df['Total_Footfall_Budget'].sum()*fraction_passed).round(0)
+            
+            
             performance_df['%Arch_FF'] = (performance_df['MTD_Actual_Footfall'] / performance_df['MTD_Budget_Footfall'])
             # Add a new column %Arch_REV as the percentage of MTD_Actual_Revenue to MTD_Budget_Revenue
             performance_df['%Arch_REV'] = (performance_df['MTD_Actual_Revenue'] / performance_df['MTD_Budget_Revenue'])
