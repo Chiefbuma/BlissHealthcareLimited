6c04dc15b974f58d14d3f71187826c347ace26ac
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-08 10:52:15 +0300

    we

--- medical.py
+++ medical.py
@@ -251,7 +251,7 @@ def app():
             Arch_Rev = (MTD_Actual_Revenue /MTD_Revenue_budget) * 100
             formatted_arch_rev = "{:.2f}%".format(Arch_Rev)
             
-            projected_revenue = performance_df['Projected_Revenue'].sum()
+            projected_revenue =Total_Budget_Reveneu*(MTD_Actual_Revenue/MTD_Revenue_budget)
             formatted_projected_reveue = "{:,.0f}".format(projected_revenue )
             
             
@@ -264,7 +264,7 @@ def app():
             Total_Budget_Footfall = performance_df['Total_Footfall_Budget'].sum()
             formatted_Total_footfall = "{:,.0f}".format(Total_Budget_Footfall)
             
-            projected_Footfall = performance_df['Projected_Footfalls'].sum()
+            projected_Footfall = formatted_Total_footfall*(MTD_Actual_Footfall/MTD_footfall_budget)
             formatted_projected_footfall = "{:,.0f}".format(projected_Footfall )
             
             Arch_Rev = (MTD_Actual_Footfall/MTD_footfall_budget) * 100
