da9a3d238f579233e13b744b6a36378547913738
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-12 16:07:22 +0300

    ew

--- maintenance.py
+++ maintenance.py
@@ -27,6 +27,17 @@ def app():
     if 'is_authenticated' not in st.session_state:
                st.session_state.is_authenticated = False
 
+    if st.session_state.is_authenticated or st.session_state.tab_clicked:
+        st.session_state.is_authenticated = True
+        st.session_state.tab_clicked = False
+        ui.tabs(options=['PyGWalker', 'Graphic Walker', 'GWalkR', 'RATH'], default_value='PyGWalker', key="kanaries")
+    else:
+        st.session_state.is_authenticated = False  
+
+    if 'is_authenticated' not in st.session_state:
+        st.session_state.is_authenticated = False 
+        # Initialize session state if it doesn't exist
+
    
     col1, col2 = st.columns([2,1])
     with col1:
@@ -105,6 +116,7 @@ def app():
 
             if choice == "Login":
                 # Check if the user is logged in
+                
                 with st.form("Login Form"):
                     st.write("Login Form")
                     staffnumber = st.text_input("Staffnumber",key="staff_medical")
@@ -154,93 +166,189 @@ def app():
                         
     if st.session_state.is_authenticated:        
         form_container.empty()  
-        if st.session_state.is_authenticated or st.session_state.tab_clicked:
-            st.session_state.is_authenticated = True
-            st.session_state.tab_clicked = False
-            ui.tabs(options=['PyGWalker', 'Graphic Walker', 'GWalkR', 'RATH'], default_value='PyGWalker', key="kanaries")
-        else:
-            st.session_state.is_authenticated = False  
-        with ui.element("div", className="flex gap-2", key="buttons_group1"):
-            ui.element("button", text="Get Started", className="btn btn-primary", key="btn1")
-            ui.element("link_button", text="Github", url="https://github.com/ObservedObserver/streamlit-shadcn-ui", variant="outline", key="btn2")
+        
+        @st.cache_resource()
+        def load_data(username, password, sharepoint_url, list_name):
+            try:
+                user_credentials = UserCredential(username, password)
+                ctx = ClientContext(sharepoint_url).with_credentials(user_credentials)
+                web = ctx.web
+                ctx.load(web)
+                ctx.execute_query()
+                target_list = ctx.web.lists.get_by_title(list_name)
+                items = target_list.get_items()
+                ctx.load(items)
+                ctx.execute_query()
 
-        st.subheader("Dashboard")
+                selected_columns= [
+                        "ID",
+                        "Title",
+                        "AuthorId",
+                        "Dateofreport",
+                        "DateNumber",
+                        "Typeofmaintenance",
+                        "Details",
+                        "Month",
+                        "FacilitycoordinatorComments",
+                        "Approvedammount",
+                        "Receivedstatus",
+                        "ReceivedAmmount",
+                        "Maintenancestatus",
+                        "ProjectsApproval",
+                        "ProjectComments",
+                        "AdminApproval",
+                        "AdminComments",
+                        "FinanceApproval",
+                        "FinanceComment",
+                        "FacilityApproval",
+                        "Approver",
+                        "LinkEdit",
+                        "AmmountontheQuotation",
+                        "Clinic2",
+                        "Region2",
+                        "CentreManager2",
+                        "Department",
+                        "RITApproval",
+                        "RITComment",
+                        "BiomedicalHeadApproval",
+                        "BiomedicalHeadComments",
+                        "MarketingManagerApproval",
+                        "MarketingManagerComments",
+                        "Phone",
+                        "Days_x0020_Pending",
+                        "Report",
+                        "Qty",
+                        "FacilityQty",
+                        "ProjectsQty",
+                        "AdminQty",
+                        "Laborcost",
+                        "MainItem",
+                        "TimeLine",
+                        "RITlabour",
+                        "FacilityLabor",
+                        "ProjectLabor",
+                        "Adminlabor",
+                        "Disbursement"
+                    ]
 
-        ui.tabs(options=['Overview', 'Analytics', 'Reports', 'Notifications'], default_value='Overview', key="main_tabs")
-
-        ui.date_picker(key="date_picker1")
-
-        cols = st.columns(3)
-        with cols[0]:
-            # with ui.card():
-            #     ui.element()
-            ui.card(title="Total Revenue", content="$45,231.89", description="+20.1% from last month", key="card1").render()
-        with cols[1]:
-            ui.card(title="Subscriptions", content="+2350", description="+180.1% from last month", key="card2").render()
-        with cols[2]:
-            ui.card(title="Sales", content="+12,234", description="+19% from last month", key="card3").render()
-
-        def generate_sales_data():
-            np.random.seed(0)  # For reproducible results
-            months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            sales = np.random.randint(1000, 5000, size=len(months))
-            return pd.DataFrame({'Month': months, 'Sales': sales})
-
-        with card_container(key="chart1"):
-            st.vega_lite_chart(generate_sales_data(), {
-                'mark': {'type': 'bar', 'tooltip': True, 'fill': 'rgb(173, 250, 29)', 'cornerRadiusEnd': 4 },
-                'encoding': {
-                    'x': {'field': 'Month', 'type': 'ordinal'},
-                    'y': {'field': 'Sales', 'type': 'quantitative', 'axis': {'grid': False}},
-                },
-            }, use_container_width=True)
-
-        # Sample data
-        data = [
-            {"invoice": "INV001", "paymentStatus": "Paid", "totalAmount": 500, "paymentMethod": "Credit Card"},
-            {"invoice": "INV002", "paymentStatus": "Unpaid", "totalAmount": 200, "paymentMethod": "Cash"},
-            {"invoice": "INV003", "paymentStatus": "Paid", "totalAmount": 150, "paymentMethod": "Debit Card"},
-            {"invoice": "INV004", "paymentStatus": "Unpaid", "totalAmount": 350, "paymentMethod": "Credit Card"},
-            {"invoice": "INV005", "paymentStatus": "Paid", "totalAmount": 400, "paymentMethod": "PayPal"},
-            # Add more records as needed
-        ]
-
-        # Creating a DataFrame
-        invoice_df = pd.DataFrame(data)
-
-        with card_container(key="table1"):
-            ui.table(data=invoice_df, maxHeight=300)
 
 
-        ui_result = ui.button("Button", key="btn")
-        st.write("UI Button Clicked:", ui_result)
+                data = []
+                for item in items:
+                    item_data = {key: item.properties[key] for key in selected_columns}
+                    data.append(item_data)
+                return pd.DataFrame(data)
+
+            except Exception as e:
+                st.error("Failed to load data from SharePoint. Please check your credentials and try again.")
+                st.error(f"Error details: {e}")
+                return None
+
+        sharepoint_url = "https://blissgvske.sharepoint.com/sites/BlissHealthcareReports"
+        list_name = "Maintenance Report"
+        username = "biosafety@blisshealthcare.co.ke"
+        password = "NaSi#2024"
+       
+        with card_container(key="Main1"):
+                        
+            Main_df = load_data(username, password, sharepoint_url, list_name)
+            
+            # Initialize session state if it doesn't exist
+        
+        
+            #Region = st.selectbox("Region:", options=[""] + list(Main_df["Region2"].unique()))
+            #Location = st.selectbox("Medical Centre:", options=[""] + list(Main_df["Clinic2"].unique()))
+            #Status = st.selectbox("Request Status:", options=[""] + list(Main_df["Maintenancestatus"].unique()))
+            #if Region == "" and Location == "" and Status == "":
+                #df_mainselected = Main_df
+            #else:
+                #df_mainselected = Main_df.query("Clinic2 == @Location or Region2 == @Region or Maintenancestatus == @Status")
+
+            Total_requests = int(Main_df.shape[0])  # Count all rows in the filtered DataFrame
+
+            # Filter the DataFrame to include only rows where "Maintenancestatus" is "Pending"
+            pending_requests_calc =  Main_df [Main_df ["Maintenancestatus"] == "Pending"]
+
+            # Count the number of rows in the filtered DataFrame
+            pending_request = int(pending_requests_calc.shape[0])
+
+            # Filter the DataFrame to include only rows where "Maintenancestatus" is "Closed"
+            closed_requests_calc =  Main_df [ Main_df ["Maintenancestatus"] == "Closed"]
+
+            # Count the number of rows in the filtered DataFrame
+            closed_request = int(closed_requests_calc.shape[0])
+
+            # Filter out rows with non-numeric values in "Days_x0020_Pending" column
+            numeric_days_pending = Main_df["Days_x0020_Pending"].apply(pd.to_numeric, errors="coerce")
+            Main_df["Days_x0020_Pending"] = numeric_days_pending
+            Main_df.dropna(subset=["Days_x0020_Pending"], inplace=True)
+
+            # Calculate average days pending
+            Average_Days_pending = int(Main_df["Days_x0020_Pending"].mean())
+            
+            
+
+            if Main_df is not None:
+                cols = st.columns(4)
+                with cols[0]:
+                    ui.card(title="Total Request", content=Total_requests, key="Revcard10", style={"height": "150px"}).render()
+                with cols[1]:
+                    ui.card(title="Closed Request", content=closed_request , key="Revcard11").render()
+                with cols[2]:
+                    ui.card(title="Pending Request", content=pending_request , key="Revcard12").render()
+                with cols[3]:
+                    ui.card(title="Average TAT:", content=Average_Days_pending, key="Revcard13").render()  
+                
+            # Display Table
+            with st.expander("View Table"):
+                st.dataframe(Main_df, use_container_width=True)
+                
+        # Define the metrics
+            metrics = [
+                {"label": "Total", "value": Total_requests},
+                {"label": "Closed", "value": closed_request},
+                {"label": "Pending", "value": pending_request},
+                {"label": "TAT(days)", "value": Average_Days_pending}
+            ]
+
+            # Create the data cards
+            fig_data_cards = go.Figure()
+
+            for i, metric in enumerate(metrics):
+                fig_data_cards.add_trace(go.Indicator(
+                    mode="number",
+                    value=metric["value"],
+                    number={'font': {'size': 25, 'color': 'white'}},
+                    domain={'row': i, 'column': 0},  # Set the row and column to stack vertically
+                    title={'text': metric["label"],'font': {'size': 20,'color': 'white'}},
+                    align="center"
+                ))
+
+            # Update layout
+            fig_data_cards.update_layout(
+                grid={'rows': len(metrics), 'columns': 1, 'pattern': "independent"},
+                template="plotly_white",
+                height=100*len(metrics),  # Adjust the height based on the number of metrics
+                paper_bgcolor='rgba(0, 131, 184, 1)',  # Set background color to transparent
+                plot_bgcolor='rgba(0, 137, 184, 1)',   # Set plot area background color to transparent
+                uniformtext=dict(minsize=40, mode='hide'),
+                margin=dict(l=20, r=20, t=50, b=5)
+                
+                )
+
+            st.markdown(
+                """
+                <style>
+                .st-cd {
+                    border: 1px solid #e6e9ef;
+                    border-radius: 5px;
+                    padding: 10px;
+                    margin-bottom: 10px;
+                }
+                </style>
+                """,
+                unsafe_allow_html=True
+)
+        
 
 
-        # Slider Component
-        slider_value = slider(default_value=[20], min_value=0, max_value=100, step=2, label="Select a Range", key="slider1")
-        st.write("Slider Value:", slider_value)
-
-        # Input Component
-        input_value = input(default_value="Hello, Streamlit!", type='text', placeholder="Enter text here", key="input1")
-        st.write("Input Value:", input_value)
-
-        # Textarea Component
-        textarea_value = textarea(default_value="Type your message here...", placeholder="Enter longer text", key="textarea1")
-        st.write("Textarea Value:", textarea_value)
-
-        # Radio Group Component
-        radio_options = [
-            {"label": "Option A", "value": "A", "id": "r1"},
-            {"label": "Option B", "value": "B", "id": "r2"},
-            {"label": "Option C", "value": "C", "id": "r3"}
-        ]
-        radio_value = radio_group(options=radio_options, default_value="B", key="radio1")
-        st.write("Selected Radio Option:", radio_value)
-
-        # Switch Component
-        switch_value = switch(default_checked=True, label="Toggle Switch", key="switch1")
-        st.write("Switch is On:", switch_value)
-
-        st.subheader("Alert Dialog")
-        trigger_btn = ui.button(text="Trigger Button", key="trigger_btn")
-        ui.alert_dialog(show=trigger_btn, title="Alert Dialog", description="This is an alert dialog", confirm_label="OK", cancel_label="Cancel", key="alert_dialog1")
