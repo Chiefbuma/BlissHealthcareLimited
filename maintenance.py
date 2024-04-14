a3d6098895288218e7d0a8f83c3bb7b044a82274
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-14 12:55:06 +0300

    wew

--- maintenance.py
+++ maintenance.py
@@ -168,7 +168,7 @@ def app():
         Dir_pending_request = int(Director_pending.shape[0])
         Dir_pending_value=Director_pending.groupby('ID')["Approved amount"].sum().sum()
         
-        Director_Approved=  Main_df [Main_df ["RIT Approval"]=="Approved"]
+        Director_Approved=  Main_df [Main_df ["RIT Approval"].str.contains("Approved", na=False)]
         Dir_Approved_request = int(Director_Approved.shape[0])
         Dir_Approved_value= Director_Approved.groupby('ID')["Approved amount"].sum().sum()
         
@@ -223,7 +223,7 @@ def app():
         
         
         data = [
-            {"Approver": "Director", "Approved No.":pending_request, "Approved Value":Dir_Approved_value, "Pending Requets":Dir_pending_request, "Pending Value":Dir_pending_value },
+            {"Approver": "Director", "Approved No.":Dir_Approved_request, "Approved Value":Dir_Approved_value, "Pending Requets":Dir_pending_request, "Pending Value":Dir_pending_value },
              {"Approver": "Projects", "Approved No.":Pro_Approved_request, "Approved Value":Pro_Approved_value, "Pending Requets":Pro_pending_request, "Pending Value":Pro_pending_value },
              {"Approver": "Facility", "Approved No.":Fac_Approved_request, "Approved Value":Fac_Approved_value, "Pending Requets":Fac_pending_request, "Pending Value":Fac_pending_value },
              {"Approver": "Operations", "Approved No.":Ops_Approved_request, "Approved Value":Ops_Approved_value, "Pending Requets":Ops_pending_request, "Pending Value":Ops_pending_value }
