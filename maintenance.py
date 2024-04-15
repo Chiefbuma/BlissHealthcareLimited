af61898beaff1647b8c10230790cc68134801581
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-15 12:27:22 +0300

    ew

--- maintenance.py
+++ maintenance.py
@@ -143,27 +143,7 @@ def app():
 
         Main_df = load_data()
 
-        @st.cache_data
-        def load_data():
-            Take_df = load_data()
-            return Take_df
         
-        Graph_df = load_data()
-        
-        # Filter the Main_df DataFrame to get the "departmental report" column
-        departmental_report_df = Graph_df["Departmental report"]
-
-        # Assuming departmental_report_df is your DataFrame
-        category_counts =  departmental_report_df.value_counts().reset_index()
-        
-            # Rename the columns to "Category" and "No."
-        category_counts.columns = ["Category", "No."]
-        
-        # Convert "No." column to integers
-        category_counts["No."] = category_counts["No."].astype(int)
-
-        # Display the new DataFrame
-        st.write(category_counts) 
          
          
                 
@@ -263,6 +243,28 @@ def app():
                         with cols[1]:
                             with card_container(key="chart1"):
                                 st.markdown("<br>", unsafe_allow_html=True)
+                                @st.cache_data
+                                def load_data():
+                                    mains = SharePoint().connect_to_list(ls_name='Maintenance Report')
+                                    return pd.DataFrame( mains)
+                            
+                                
+                                Graph_df = load_data()
+                                
+                                # Filter the Main_df DataFrame to get the "departmental report" column
+                                departmental_report_df = Graph_df["Departmental report"]
+
+                                # Assuming departmental_report_df is your DataFrame
+                                category_counts =  departmental_report_df.value_counts().reset_index()
+                                
+                                    # Rename the columns to "Category" and "No."
+                                category_counts.columns = ["Category", "No."]
+                                
+                                # Convert "No." column to integers
+                                category_counts["No."] = category_counts["No."].astype(int)
+
+                                # Display the new DataFrame
+                                st.write(category_counts) 
                                 
                                 st.vega_lite_chart(category_counts, {
                                     'mark': {'type': 'bar', 'tooltip': True, 'fill': 'black', 'cornerRadiusEnd': 6,'width': 'container',
