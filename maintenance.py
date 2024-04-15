4756336017a497e94f50effe05cd4ccd9ea05139
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-15 16:00:33 +0300

    ew

--- maintenance.py
+++ maintenance.py
@@ -269,9 +269,34 @@ def app():
 
                                     Main_df = load_data()
                                     # Display the gallery-like table
-                                    
-                                ui.table(data= Main_df, maxHeight=300)        
-                                    
+                                    for index, row in Main_df.iterrows():
+                                       Item={index + 1}
+                                       ID= {row['ID']}
+                                       Approver= {row['Approver']}
+                                       Link= {row['LinkEdit']}
+                                        # Add more columns as needed
+
+                                        # Add a button for each row item
+                                    button_clicked = st.button(f"Process Item {index + 1}")
+                                    if button_clicked:
+                                        st.write(f"Processing item {index + 1}...")
+                                        
+                                        
+                                        st.write("---")
+                                        
+                                        data = [
+                                            {"Approver": Approver,
+                                             "Item":Item, 
+                                             "Value":Dir_Approved_value,
+                                             "ID": ID,
+                                             "Link": Link },
+                                        ]   
+                                        
+                                        final_df=pd.DataFrame(data)
+                                        
+                                                 
+                                ui.table(data= final_df, maxHeight=300)        
+                                        
                                         
                                          
                     metrics = [
