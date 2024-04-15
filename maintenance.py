cce6cfb1a7358743eabc4fc2ca6e5c4ef21c6a15
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-15 17:11:32 +0300

    ew

--- maintenance.py
+++ maintenance.py
@@ -262,46 +262,22 @@ def app():
                              ui.table(data=Approval_df, maxHeight=300)
                         with cols[1]:
                                 with card_container(key="gallery1"):
-                                   
-                                    data_df = pd.DataFrame(
-                                                {
-                                                    "apps": [
-                                                        "https://roadmap.streamlit.app",
-                                                        "https://extras.streamlit.app",
-                                                        "https://issues.streamlit.app",
-                                                        "https://30days.streamlit.app",
-                                                    ],
-                                                    "creator": [
-                                                        "https://github.com/streamlit",
-                                                        "https://github.com/arnaudmiribel",
-                                                        "https://github.com/streamlit",
-                                                        "https://github.com/streamlit",
-                                                    ],
-                                                }
-                                            )
+                                                                        
+                                    data_df = Main_df[['LinkEdit']]
 
                                     st.data_editor(
                                         data_df,
                                         column_config={
-                                            "apps": st.column_config.LinkColumn(
-                                                "Trending apps",
-                                                help="The top trending Streamlit apps",
-                                                validate="^https://[a-z]+\.streamlit\.app$",
+                                            "LinkEdit": st.column_config.LinkColumn(
+                                                "LinkEdit",
+                                                validate="^https://[a-z]+\.apps\.powerapps$",
                                                 max_chars=100,
-                                                display_text="https://(.*?)\.streamlit\.app"
+                                                display_text="Approve here"
                                             ),
-                                            "creator": st.column_config.LinkColumn(
-                                                "App Creator", display_text="Open profile"
-                                            ),
+                                            
                                         },
                                         hide_index=True,
-                                            )
-                                    
-                                                                        # Format the Link column as clickable links
-                                    data_df['apps'] =  data_df['apps'].apply(lambda x: f'<a href="{x}">{x}</a>')
-                                    
-                                   
-                                    ui.table(data=data_df,format={'Link': 'raw'}, maxHeight=300)
+                                    )
                                     
                                          
                     metrics = [
