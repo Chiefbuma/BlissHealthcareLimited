585a5c4a576810a5631bfbd378cdc9a3641f6fe7
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-21 14:01:20 +0300

    er

--- maintenance.py
+++ maintenance.py
@@ -153,7 +153,7 @@ def app():
     if st.session_state.is_authenticated:
         form_container.empty()
         # get clients sharepoint list
-        @st.cache_data
+     
         def load_data():
                 clients = SharePoint().connect_to_list(ls_name='Maintenance Report')
                 return pd.DataFrame(clients)
@@ -337,7 +337,7 @@ def app():
                 with card_container(key="gallery1"):
                     
                     st.markdown('<div style="height: 0px; overflow-y: scroll;">', unsafe_allow_html=True)
-                    @st.cache_data
+                    
                     def load_data():
                             New = SharePoint().connect_to_list(ls_name='Maintenance Report')
                             return pd.DataFrame(  New )
