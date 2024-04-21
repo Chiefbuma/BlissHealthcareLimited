00bed3d2d2f9f70f3800552370c3f49565d0a5d2
Chiefbuma <jorgebuma.jb@gmail.com>
2024-04-21 12:42:17 +0300

    ew

--- maintenance.py
+++ maintenance.py
@@ -306,12 +306,12 @@ def app():
                                 return pd.DataFrame({'Month': Item, 'Sales': Cost})
                             with card_container(key="chart1"):
                                 st.vega_lite_chart(generate_sales_data(), {
-                                'mark': {'type': 'bar', 'tooltip': True, 'fill': 'rgb(173, 250, 29)', 'cornerRadiusEnd': 4},
-                                'encoding': {
-                                    'x': {'field': 'Item', 'type': 'ordinal'},
-                                    'y': {'field': 'Cost', 'type': 'quantitative', 'sort': '-x', 'axis': {'grid': False}},
-                                },
-                            }, use_container_width=True)
+                                    'mark': {'type': 'bar', 'tooltip': True, 'fill': 'rgb(173, 250, 29)', 'cornerRadiusEnd': 4 },
+                                    'encoding': {
+                                        'x': {'field': 'Item', 'type': 'ordinal'},
+                                        'y': {'field': 'Cost', 'type': 'quantitative', 'axis': {'grid': False}},
+                                    },
+                                }, use_container_width=True)
                         with card_container(key="table1"):
                              ui.table(data=Approval_df, maxHeight=300)
                             
