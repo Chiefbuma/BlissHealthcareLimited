d6d521be9313986b836af0983b3dab9064d8b8b2
Chiefbuma <jorgebuma.jb@gmail.com>
2024-05-13 10:28:19 +0300

    ew

--- maintenance.py
+++ maintenance.py
@@ -18,4 +18,24 @@ from IPython.display import HTML
 from streamlit_dynamic_filters import DynamicFilters
 
 def app():
-   st.write("Hello")
+    if 'is_authenticated' not in st.session_state:
+        st.session_state.is_authenticated = False
+        form_container = st.empty()
+        
+        col1, col2 = st.columns([2,1])
+        with col1:
+            menu = ["Login", "Sign up"]
+        choice = st.sidebar.selectbox("", menu,key="choice_medical")
+        with form_container:
+            @st.cache_data
+            def init_connection():
+                url = "https://effdqrpabawzgqvugxup.supabase.co"
+                key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmZmRxcnBhYmF3emdxdnVneHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MTQ1NDYsImV4cCI6MjAyNjA5MDU0Nn0.Dkxicm9oaLR5rm-SWlvGfV5OSZxFrim6x8-QNnc2Ua8"
+                return create_client(url, key)
+
+            supabase = init_connection()
+
+            response = supabase.table('facilities').select("*").execute()
+
+            location_df = pd.DataFrame(response.data)
+            st.write(location_df)
