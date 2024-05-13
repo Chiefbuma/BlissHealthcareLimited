d7d786fcbd83e2a7d4aa4c021ab4ef51e5381fce
Chiefbuma <jorgebuma.jb@gmail.com>
2024-05-13 11:04:02 +0300

    ew

--- Buma.py
+++ Buma.py
@@ -1,6 +1,24 @@
 import streamlit as st
 from shareplum import Site, Office365
 from shareplum.site import Version
+import streamlit as st
+from st_supabase_connection import SupabaseConnection
+from supabase import create_client, Client
+import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
+import plotly.graph_objects as go
+from office365.sharepoint.client_context import ClientContext
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.sharepoint.client_context import UserCredential
+import streamlit_option_menu as option_menu
+import streamlit_shadcn_ui as ui
+from local_components import card_container
+from streamlit_shadcn_ui import slider, input, textarea, radio_group, switch
+from sharepoint import SharePoint
+import main
+from postgrest import APIError
+from IPython.display import HTML
 
 USERNAME = "biosafety@blisshealthcare.co.ke"
 PASSWORD = "Buma#2025"
