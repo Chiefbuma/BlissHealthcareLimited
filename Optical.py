4cf5bd0c6d661f5580c82e73d660df9502ffde21
Chiefbuma <jorgebuma.jb@gmail.com>
2024-06-19 13:18:08 +0300

    ew

--- Optical.py
+++ Optical.py
@@ -18,7 +18,6 @@ from IPython.display import HTML
 import main
 from streamlit_dynamic_filters import DynamicFilters
 from streamlit_gsheets import GSheetsConnection
-from google.oauth2.service_account import Credential
 from urllib.error import HTTPError
 from google.oauth2.service_account import Credentials
 
