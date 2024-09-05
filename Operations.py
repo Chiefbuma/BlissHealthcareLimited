2b3d98bf8ea07e903ec6ceca1e5270d80c372f15
Chiefbuma <jorgebuma.jb@gmail.com>
2024-09-05 17:43:37 +0300

    ew

--- Operations.py
+++ Operations.py
@@ -74,7 +74,7 @@ def app():
                 # Define the iframe code with expanded width and centered alignment
                 iframe_code = """
                 <div style="display: flex; justify-content: center;">
-                    <iframe title="DASHBOARD - END MONTH" 
+                    <iframe title="DASHBOARD - MTD | Summary" 
                             width="1100" 
                             height="700" 
                             src="https://app.powerbi.com/reportEmbed?reportId=16f6496e-6f9e-4b0c-8417-9291ff93938c&autoAuth=true&ctid=e2fcf853-8bfc-47b9-812f-359fb0a13c63&pageName=ReportSection679615f8e34604a7e581&navContentPaneEnabled=false&filterPaneEnabled=false&bookmarkPaneEnabled=false&navigation=false" 
