49779b2bde2143135891fe58b913d46110f12c67
Chiefbuma <jorgebuma.jb@gmail.com>
2024-09-06 15:05:21 +0300

    ew

--- Operations.py
+++ Operations.py
@@ -82,7 +82,7 @@ def app():
                 # Display the iframe in Streamlit
                 st.markdown(iframe_code, unsafe_allow_html=True)
 
-            with st.expander("YDT"):   
+            
                
                 iframe_code = """
                 <div style="display: flex; justify-content: center;">
@@ -98,25 +98,33 @@ def app():
                 # Display the iframe in Streamlit
                 st.markdown(iframe_code, unsafe_allow_html=True)
                 
-            with st.expander("QTR"):
-                #iframe code with expanded width and centered alignment
-                iframe_code = """
-                <div style="display: flex; justify-content: center;">
-                    <iframe title="DASHBOARD - QRT | Summary" 
-                            width="1100" 
-                            height="700" 
-                            src="https://app.powerbi.com/reportEmbed?reportId=16f6496e-6f9e-4b0c-8417-9291ff93938c&autoAuth=true&ctid=e2fcf853-8bfc-47b9-812f-359fb0a13c63&pageName=ReportSectione1f47eff2818000c1b5e&navContentPaneEnabled=false&filterPaneEnabled=false&bookmarkPaneEnabled=false&navigation=false" 
-                            frameborder="0" 
-                            allowFullScreen="true">
-                    </iframe>
-                </div>
-                """
-                # Display the iframe in Streamlit
-                st.markdown(iframe_code, unsafe_allow_html=True)
-            
-            
-            with st.expander("Annual"):
-                #iframe code with expanded width and centered alignment
+            # Checkbox to toggle the visibility of the container
+            show_container = st.checkbox("Show container", value=True)
+
+            # Conditional rendering of the container
+            if show_container:
+                with st.container():
+                    st.write("This is a container that can be hidden or shown.")
+                
+                    #iframe code with expanded width and centered alignment
+                    iframe_code = """
+                    <div style="display: flex; justify-content: center;">
+                        <iframe title="DASHBOARD - QRT | Summary" 
+                                width="1100" 
+                                height="700" 
+                                src="https://app.powerbi.com/reportEmbed?reportId=16f6496e-6f9e-4b0c-8417-9291ff93938c&autoAuth=true&ctid=e2fcf853-8bfc-47b9-812f-359fb0a13c63&pageName=ReportSectione1f47eff2818000c1b5e&navContentPaneEnabled=false&filterPaneEnabled=false&bookmarkPaneEnabled=false&navigation=false" 
+                                frameborder="0" 
+                                allowFullScreen="true">
+                        </iframe>
+                    </div>
+                    """
+                    # Display the iframe in Streamlit
+                    st.markdown(iframe_code, unsafe_allow_html=True)
+                
+                
+              
+            else:
+                st.write("The container is hidden.")
                 iframe_code = """
                 <div style="display: flex; justify-content: center;">
                     <iframe title="DASHBOARD - Annual | Summary" 
@@ -130,10 +138,7 @@ def app():
                 """
                 # Display the iframe in Streamlit
                 st.markdown(iframe_code, unsafe_allow_html=True)
-                
-            
         
-            
             
     else:
         st.write("You are not logged in. Click **[Account]** on the side menu to Login or Signup to proceed")
