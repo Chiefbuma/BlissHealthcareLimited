6fd8be2693c3297b92408aa49ffce26294ba19ec
Chiefbuma <jorgebuma.jb@gmail.com>
2024-10-25 17:37:34 +0300

    ew

--- TAT.py
+++ TAT.py
@@ -3,6 +3,29 @@ from supabase import create_client, Client
 import pandas as pd
 from datetime import datetime
 from local_components import card_container
+import streamlit as st
+from st_supabase_connection import SupabaseConnection
+from supabase import create_client, Client
+import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
+import plotly.graph_objects as go
+from office365.sharepoint.client_context import ClientContext
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.sharepoint.client_context import UserCredential
+import streamlit_option_menu as option_menu
+import streamlit_shadcn_ui as ui
+from local_components import card_container
+from streamlit_shadcn_ui import slider, input, textarea, radio_group, switch
+from sharepoint import SharePoint
+from postgrest import APIError
+from IPython.display import HTML
+import main
+from streamlit_dynamic_filters import DynamicFilters
+import time
+import os
+from supabase import create_client, Client
+from datetime import datetime
 
 def app():
     
@@ -14,10 +37,7 @@ def app():
                 </span>""", unsafe_allow_html=True)
         
     if st.session_state.is_authenticated:
-        # Supabase credentials
-        url = "https://effdqrpabawzgqvugxup.supabase.co"
-        key = "YOUR_SUPABASE_API_KEY"  # Replace with your actual Supabase API key
-        supabase: Client = create_client(url, key)
+        
 
         with card_container("Upload"):
             st.header('Dispatch Packages🔖')
