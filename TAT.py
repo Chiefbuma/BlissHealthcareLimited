55931ba1ff2fd60e0bb6c7a6e3c0432507aeb401
Chiefbuma <jorgebuma.jb@gmail.com>
2024-10-26 10:04:34 +0300

    ew

--- TAT.py
+++ TAT.py
@@ -153,7 +153,7 @@ def app():
                 pivoted_df = grouped_df.pivot_table(
                     index='FacilityName',           # Rows as medical centers
                     columns='Shift',                # Columns as shifts
-                    values='Average_TAT_Hours',           # Values as Average TAT
+                    values='Average_TAT',           # Values as Average TAT
                     aggfunc='mean'                  # Average in case of multiple entries
                 )
 
