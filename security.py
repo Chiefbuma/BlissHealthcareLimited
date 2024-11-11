cdbfd4fe7f26cd8a82eacabdfa825053e765d89e
Chiefbuma <jorgebuma.jb@gmail.com>
2024-11-12 00:20:37 +0300

    ew

--- security.py
+++ security.py
@@ -37,7 +37,7 @@ def app():
         def init_connection():
            url = "https://jxdfovectoeugyeixnnf.supabase.co"
            key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZGZvdmVjdG9ldWd5ZWl4bm5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzMDc3MTUsImV4cCI6MjA0Njg4MzcxNX0.eanuttDhkje4q43yyicxEEJMUnDO5aGaIyJYtKFUu3Y"
-            return create_client(url, key)
+           return create_client(url, key)
 
         supabase = init_connection()
         
