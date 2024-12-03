90078c3c03ddcc97262aa57160b797f0819933bd
Chiefbuma <jorgebuma.jb@gmail.com>
2024-12-03 13:15:40 +0300

    ew

--- Operations.py
+++ Operations.py
@@ -33,39 +33,86 @@ def app():
     if st.session_state.is_authenticated:
         location=st.session_state.Region
       
+     
         @st.cache_resource
         def init_connection():
-            url = "https://effdqrpabawzgqvugxup.supabase.co"
-            key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmZmRxcnBhYmF3emdxdnVneHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MTQ1NDYsImV4cCI6MjAyNjA5MDU0Nn0.Dkxicm9oaLR5rm-SWlvGfV5OSZxFrim6x8-QNnc2Ua8"
-            return create_client(url, key)
+           url = "https://jxdfovectoeugyeixnnf.supabase.co"
+           key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZGZvdmVjdG9ldWd5ZWl4bm5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzMDc3MTUsImV4cCI6MjA0Njg4MzcxNX0.eanuttDhkje4q43yyicxEEJMUnDO5aGaIyJYtKFUu3Y"
+
+           return create_client(url, key)
+            
 
         supabase = init_connection()
         
         # Check if the connection is successful
         if init_connection():
+        
             
-            st.session_state.logged_in = True
+            st.session_state.logged_in= True
+            # Dropdown for selecting the year
+        
+        
+            #Get the previous month as a date
+            previous_month_date = datetime.now() - relativedelta(months=1)
+
+            
             current_month = datetime.now().month 
             current_month_name = datetime.now().strftime("%B")
             
+            #current_month = datetime.now() - relativedelta(months=1)
+            #current_month_name = (datetime.now() - relativedelta(months=1)).strftime("%B")
+            
+
             # Query the MTD_Revenue table with the filter for location_name and Month
-            response = supabase.from_('MTD_Revenue').select('*').eq('location_name', location).eq('Month', current_month_name).execute()
+            response = supabase.from_('MTD_Revenue').select('*').eq('location_name', location).eq('Month', current_month_name ).execute()
             performance_df = pd.DataFrame(response.data)
+# The above code is using multi-line comments in Python, which are denoted by three consecutive pound
+# signs (
+            
+            # Query the MTD_Revenue table with the filter for location_name and Month
+            Allresponse = supabase.from_('MTD_Revenue').select('*').eq('location_name', location).execute()
+            Allperformance_df = pd.DataFrame(Allresponse.data)
+            
             
             Lastdateresponse = supabase.from_('Last_Update').select('*').execute()
             LastUpdate_df = pd.DataFrame(Lastdateresponse.data)
-            LastUpdate_df = LastUpdate_df[['Last_Updated']]  
+            LastUpdate_df = LastUpdate_df[['Last_Updated']]  # Assuming 'Last_Updated' is the column you want
             Lastdate = LastUpdate_df.iloc[0]['Last_Updated']
           
             # Define the function to calculate the fraction of days passed in a month
             def fraction_of_days_in_month(date):
+                # Calculate the total number of days in the month
                 total_days_in_month = (date.replace(day=1) + timedelta(days=32)).replace(day=1) - timedelta(days=1)
+                
+                # Calculate the fraction of days passed
                 fraction_passed = (date.day) / total_days_in_month.day
+                
                 return fraction_passed
            
-            fraction_passed = fraction_of_days_in_month(datetime.strptime(Lastdate, "%Y-%m-%d").date())
+            # Create a new figure
+            fig3 = go.Figure()
+            
+            # # Define the metrics
+                        # Calculate the previous day
+            Lastdate = LastUpdate_df.iloc[0]['Last_Updated']
+            Lastdate_date = datetime.strptime(Lastdate, "%Y-%m-%d").date()
             
             
+            # Convert Lastdate to a datetime.date object
+            dateword = datetime.strptime(Lastdate, "%Y-%m-%d").date()
+
+            # Format the date as "Friday 24th 2024"
+            formatted_date = dateword.strftime("%A %dth %Y")
+
+            # Calculate fraction of days passed for the selected month
+            fraction_passed = fraction_of_days_in_month(Lastdate_date)
+            
+            
+            
+            
+            #Total_budget_FF = performance_df['Budget_Footfall'].sum()
+            #formatted_FF_budget = "{:,.0f}".format(Total_budget_FF)
+                                    
             container = st.container(border=True, height=720)
                                 
             with container:
