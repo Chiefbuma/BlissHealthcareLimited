cd34e838903f5e7e7c499ebc77ae05f56661b233
Chiefbuma <jorgebuma.jb@gmail.com>
2025-01-03 01:09:38 +0300

    ew

--- medical.py
+++ medical.py
@@ -64,7 +64,7 @@ def app():
             
 
             # Query the MTD_Revenue table with the filter for location_name and Month
-            response = supabase.from_('MTD_Revenue').select('*').eq('location_name', location).eq('Month', current_month_name ).execute()
+            response = supabase.from_('MTD_Revenue').select('*').eq('location_name', location).eq('Month', 'December' ).execute()
             performance_df = pd.DataFrame(response.data)
 # The above code is using multi-line comments in Python, which are denoted by three consecutive pound
 # signs (
