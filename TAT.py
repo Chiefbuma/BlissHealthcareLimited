9ff21f2eb37528795b67aecec0641f23eb421a76
Chiefbuma <122814097+Chiefbuma@users.noreply.github.com>
2025-04-30 09:35:27 +0300

    Update TAT.py

--- TAT.py
+++ TAT.py
@@ -47,13 +47,15 @@ def app():
 
         with card_container("Upload"):
 
-            st.write(
-                "[Click here to Download TAT Dump ](https://app.blissmedicalcentre.com/dashboard/DashBoard.aspx?name=ConsolidatedTATReportNew)")
+           
             st.write("Username:10443")
             st.write("Password:123")
             st.header('Turn Around Time(TAT)🔖')
             # Upload CSV file
             uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+             st.write(
+                "[Click here to Download TAT Dump ](https://app.blissmedicalcentre.com/dashboard/DashBoard.aspx?name=ConsolidatedTATReportNew)")
 
             if uploaded_file is not None:
                 # Define the columns to import and parse as datetime
